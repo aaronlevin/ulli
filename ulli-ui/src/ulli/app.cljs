4129bbfd33e65de1b4fbfc076529f62b06c76488
aaron levin <vilevin@gmail.com>
2014-07-19 17:16:40 -0400

    Refactor :items for `draggable`

--- ulli-ui/src/ulli/app.cljs
+++ ulli-ui/src/ulli/app.cljs
@@ -6,9 +6,9 @@
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [secretary.core :as secretary]
+            [ulli.draggable :as draggable]
             [ulli.utils :refer [pluralize now guid store hidden]]
-            [clojure.string :as string]
-            [ulli.item :as item])
+            [clojure.string :as string])
   (:import [goog History]
            [goog.history EventType]))
 
@@ -18,7 +18,8 @@
 
 (def app-state (atom {:description "my description"
                       :title "my ulli x2"
-                      :items [] }))
+                      :sort []
+                      :items {}}))
 
 ;; =============================================================================
 ;; Ulli :: Title 
@@ -76,39 +77,39 @@
 
 ;; ============================================================================
 ;; Ulli :: Item
-(defn ulli-item-view [{:keys [id text] :as item} owner]
+(defn ulli-item-view [item owner]
   (reify
     om/IWillMount
     (will-mount [_]
-      (let [item-update-chan (chan)]
+      (let [item-update-chan (chan)
+            text (:text (first (vals item)))]
         (om/set-state! owner :item-update-chan item-update-chan)
         (go (while true
               (let [text (<! item-update-chan)]
-                (om/update! item :text text))))))
+                (om/update! item [vals first :text] text))))))
     om/IRenderState
     (render-state [e {:keys [item-update-chan delete-channel]}]
-      (dom/div #js {:draggable true
-                    :onDragStart (fn [e]
-                                   (prn "drag start")
-                                   (prn (.. e js/dataTransfer js/dropEffect)))}
-               (dom/p #js {:id id
-                           :contentEditable true
-                           :onInput (fn [_]
-                                      (let [node (.-firstChild (om/get-node owner))]
-                                        (when node
-                                          (put! item-update-chan (.-nodeValue node)))))
-                           :onBlur (fn [_]
-                                     (let [node (.-firstChild (om/get-node owner))]
-                                       ;; TODO: very first blur event causes error
-                                       ;; why?
-                                       (when node
-                                         (put! item-update-chan (.-nodeValue node)))))} text)
-               (dom/button #js {:type "button"
-                                :className "close"
-                                :onClick (fn [e] (put! delete-channel item))
-                                }
-                               (dom/span #js {:aria-hidden "true"} "x")
-                               (dom/span #js {:className "sr-only"} "Close"))))))
+      (let [id (:id item)
+            text (:text item)]
+        (dom/div nil
+                 (dom/p #js {:id id
+                             :contentEditable true
+                             :onInput (fn [_]
+                                        (let [node (.-firstChild (om/get-node owner))]
+                                          (when node
+                                            (put! item-update-chan (.-nodeValue node)))))
+                             :onBlur (fn [_]
+                                       (let [node (.-firstChild (om/get-node owner))]
+                                         ;; TODO: very first blur event causes error
+                                         ;; why?
+                                         (when node
+                                           (put! item-update-chan (.-nodeValue node)))))} text)
+                 (dom/button #js {:type "button"
+                                  :className "close"
+                                  :onClick (fn [e] (put! delete-channel item))
+                                  }
+                             (dom/span #js {:aria-hidden "true"} "x")
+                             (dom/span #js {:className "sr-only"} "Close")))))))
 
 ;; =============================================================================
 ;; Main and Footer components
@@ -117,14 +118,23 @@
   (when (== (.-which e) ENTER_KEY)
     (let [new-field (om/get-node owner "newListItem")]
       (when-not (string/blank? (.. new-field -value trim))
-        (let [new-list-item {:id (guid)
-                             :text (.-value new-field)
-                             :completed false}]
+        (let [sort-channel (om/get-state owner :sort-channel)
+              new-id (guid)
+              new-list-item {new-id {:id new-id 
+                                  :text (.-value new-field)
+                                  :completed false}}]
           (om/transact! app :items
-                        #(cons new-list-item %)
-                        [:create new-list-item]))
+                        #(merge new-list-item %)
+                        [:create new-list-item])
+          (put! sort-channel (conj (:sort @app) new-id))
+          (om/transact! app :sort
+                        #(conj % new-id)))
         (set! (.-value new-field) "")))
     false))
+
+(defn item-view-curry [chan]
+  (fn [the-item owner]
+    (om/build ulli-item-view the-item {:init-state {:delete-channel chan}})))
 
 
 ;; Main ulli app. TODO: do something on the event-channel.
@@ -132,10 +142,12 @@
   (reify
     om/IWillMount
     (will-mount [_]
-      (let [event-channel (chan)
+      (let [sort-channel (chan)
+            event-channel (chan)
             delete-channel (chan)]
         (om/set-state! owner :event-channel event-channel)
         (om/set-state! owner :delete-channel delete-channel)
+        (om/set-state! owner :sort-channel sort-channel)
         (go (while true
               (let [[type value] (<! event-channel)]
                 ;; handle-event goes here
@@ -163,9 +175,31 @@
                                                                     :id "list-item"
                                                                     :placeholder ""
                                                                     :onKeyDown #(handle-new-item-keydown % app owner)})))
-                                 (apply dom/ol nil
-                                        (map (fn [item] (dom/li nil
-                                                                (om/build ulli-item-view item {:init-state {:delete-channel delete-channel}}))) items))))))))
+                                 (om/build draggable/sortable app
+                                           {:init-state {:view (item-view-curry delete-channel)
+                                                         :sort-chan (om/get-state owner :sort-channel)}})))))))
+                                 ;;(apply dom/ol nil
+                                        ;;(map (fn [item] 
+                                               ;;(om/build (dom/li nil
+                                                                ;;(om/build ulli-item-view item {:init-state {:delete-channel delete-channel}}))) items))))))))
+
+;; ============================================================================
+
+(defn sortable-item [the-item owner]
+  (om/component (dom/span nil (str "Item " (:text the-item)))))
+
+(defn sortable-view [app owner]
+  (om/component
+    (dom/div nil
+      (dom/h2 nil "Sortable example")
+      (om/build draggable/sortable app {:init-state {:view sortable-item}}))))
+
+(def app-state-2
+  (let [items (->> (take 10 (map vector (repeatedly guid) (range)))
+                (map (fn [[id n]] [id {:id id :text n}]))
+                (into {}))]
+    (atom {:items items
+           :sort (into [] (keys items))})))
 
 ;; =============================================================================
 ;; Routing
@@ -177,6 +211,10 @@
     (secretary/set-html! app-node "<h1>hello</h1>"))
 
 (defroute "/" [] app-state
+  (prn app-state)
+  (prn app-state-2)
+  ;;(om/root sortable-view app-state-2
+           ;;{:target (.getElementById js/document "main")}))
   (om/root ulli-app app-state
            {:target (.getElementById js/document "main")}))
 
