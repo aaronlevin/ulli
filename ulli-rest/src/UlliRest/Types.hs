8049e29f8cb7d4ac8ded8336d38db74aa4d7a4f0
aaron levin <vilevin@gmail.com>
2014-07-25 02:00:10 -0400

    JSON instances for the List Algebra

--- ulli-rest/src/UlliRest/Types.hs
+++ ulli-rest/src/UlliRest/Types.hs
@@ -1,53 +1,111 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module UlliRest.Types ( delete
-  , get
   , insertAt
   , listInterpreter
   , noop
-  , pop
   , push
   , set) where
 
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (mzero)
 import Control.Monad.Free (Free (Free, Pure))
+import Data.Aeson ((.:), (.=), FromJSON(..), object, ToJSON(..), Value(Null, Object, String))
 import Data.Functor()
+-- import Data.Text (Text)
 
-data ListEdit a next = Push a next
-                     | InsertAt Int a next
-                     | Set Int a next
-                     | Delete Int next
-                     | Pop (Maybe a -> next)
-                     | Get Int (Maybe a -> next)
+-- | Algebra operations as basic data types for simple pattern matching
+data ListAlgOp = PushEdit
+               | InsertAtEdit
+               | SetEdit
+               | DeleteEdit
 
-instance Functor (ListEdit a) where
+instance ToJSON ListAlgOp where
+  toJSON PushEdit     = String "push"
+  toJSON InsertAtEdit = String "insert"
+  toJSON SetEdit      = String "set"
+  toJSON DeleteEdit   = String "delete"
+
+instance FromJSON ListAlgOp where
+  parseJSON (String "push")   = return PushEdit
+  parseJSON (String "insert") = return InsertAtEdit
+  parseJSON (String "set")    = return SetEdit
+  parseJSON (String "delete") = return DeleteEdit
+  parseJSON _                 = mzero
+
+-- | an Algebra of seralizable list operations
+data ListAlg a next = Push a next
+                    | InsertAt Int a next
+                    | Set Int a next
+                    | Delete Int next
+                    | Noop
+
+-- | We can serialize the List Algebra in two ways: a tree structure 
+-- | or a list structure. The only difference is the tree structure has
+-- | the next instruction in the JSON document, whereas the list structure
+-- | implicitly has the next instruction behind it in a list. 
+-- | therefore, we use two newtypes to give serialized versions of both these.
+-- newtype ListAlgTree a next = ListAlgTree { unListAlgTree :: ListAlg a next }
+
+instance (ToJSON a, ToJSON next) => ToJSON (ListAlg a next) where
+  toJSON (Push a next) = object [ "edit" .= PushEdit
+                                , "value" .= a
+                                , "next" .= next
+                                ]
+
+  toJSON (InsertAt i a next) = object [ "edit" .= InsertAtEdit
+                                      , "value" .= a
+                                      , "index" .= i
+                                      , "next" .= next
+                                      ]
+
+  toJSON (Set i a next) = object [ "edit" .= SetEdit
+                                 , "value" .= a
+                                 , "index" .= i
+                                 , "next" .= next
+                                 ]
+
+  toJSON (Delete i next) = object [ "edit" .= DeleteEdit
+                                  , "index" .= i
+                                  , "next" .= next
+                                  ]
+
+  toJSON (Noop) = Null
+
+instance (FromJSON a, FromJSON next) => FromJSON (ListAlg a next) where
+  parseJSON (Object v) = (v .: "edit") >>= handleEdit where
+    handleEdit e = case e of
+      PushEdit      -> Push <$> v .: "value" <*> v .: "next"
+      InsertAtEdit  -> InsertAt <$> v .: "value" <*> v .: "index" <*> v .: "next"
+      SetEdit       -> Set <$> v .: "value" <*> v .: "index" <*> v .: "next"
+      DeleteEdit    -> Delete <$> v .: "index" <*> v .: "next"
+
+  parseJSON _ = mzero
+
+instance Functor (ListAlg a) where
   fmap f (Push a n) = Push a (f n)
   fmap f (InsertAt i a n) = InsertAt i a (f n)
   fmap f (Set i a n) = Set i a (f n)
-  fmap f (Pop g) = Pop (f . g)
   fmap f (Delete i n) = Delete i (f n)
-  fmap f (Get i g) = Get i (f . g)
+  fmap _ Noop = Noop
 
-push :: a -> Free (ListEdit a) ()
+push :: a -> Free (ListAlg a) ()
 push a = Free (Push a (Pure ()))
 
-insertAt :: Int -> a -> Free (ListEdit a) ()
+insertAt :: Int -> a -> Free (ListAlg a) ()
 insertAt i a = Free (InsertAt i a (Pure ()))
 
-set :: Int -> a -> Free (ListEdit a) ()
+set :: Int -> a -> Free (ListAlg a) ()
 set i a = Free (Set i a (Pure ()))
 
-delete :: Int -> Free (ListEdit a) ()
+delete :: Int -> Free (ListAlg a) ()
 delete i = Free (Delete i (Pure ()))
 
-pop :: (Maybe a -> Free (ListEdit a) ()) -> Free (ListEdit a) ()
-pop f = Free (Pop f)
-
-get :: Int -> (Maybe a -> Free (ListEdit a) ()) -> Free (ListEdit a) ()
-get i f = Free (Get i f)
-
-noop :: Free (ListEdit a) ()
+noop :: Free (ListAlg a) ()
 noop = Pure ()
 
 -- | we assume the index starts at 0
-listInterpreter :: Free (ListEdit a) () -> [a]
+listInterpreter :: Free (ListAlg a) () -> [a]
 listInterpreter = go [] where
 
   go as (Free (Push a n)) = go (a:as) n
@@ -65,13 +123,6 @@ listInterpreter = go [] where
       (_,[]) -> as
       (h,_:ts) -> h ++ ts
 
-  go [] (Free (Pop g)) = go [] (g Nothing)
-  go as@(a:_) (Free (Pop g)) = go as (g (Just a))
-
-  go as (Free (Get i g)) = go as (g element) where
-    element = case splitAt i as of
-      (_,[]) -> Nothing
-      (_,x:_) -> Just x
+  go as (Free Noop) = as
 
   go as (Pure ()) = as
- 
